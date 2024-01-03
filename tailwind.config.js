84e658a81e44f0766605d4096f10efd2f0fdd873
ravi <ravikr845430@gmail.com>
2024-01-03 17:52:56 +0530

    copied from namastey-react lesson_11

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
