4ea13e6cd741b78830c4382a079ad705c6c4e96d
ravi kumar <ravi.kumar07@telusinternational.com>
2025-05-07 14:34:15 +0530

    revising react

--- revision.js
+++ revision.js
@@ -1,7 +1,5 @@
 /*
-    1) CDN  ==>   Done
-    2) ControlledVsUncontrolled Components  ==> Done
-    3) CORS ==> Pending
-    4) Custom Hooks ==> Done
-    5) Real and Virtual DOM
+    ==> React-Book.js ==> Done
+    ==> CDN ==> Inprogress
+
 */
